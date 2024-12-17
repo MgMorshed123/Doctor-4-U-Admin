b61203b2e52e9dc50d3646e3768e50c490c1c53a
mdarf <mdarfinji45@gmail.com>
2024-12-17 20:20:32 +0800

    backedn url added

--- src/context/AdminContext.jsx
+++ src/context/AdminContext.jsx
@@ -1,9 +1,17 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const AdminContext = createContext();
 
 const AdminContextProvider = (props) => {
-  const value = {};
+  const [aToken, setAToken] = useState("");
+
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+  const value = {
+    aToken,
+    backendUrl,
+    setAToken,
+  };
 
   return (
     <AdminContext.Provider value={value}>
