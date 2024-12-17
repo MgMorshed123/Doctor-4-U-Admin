48288954db2cc17ee55a943e25d18e85645c2d1d
mdarf <mdarfinji45@gmail.com>
2024-12-17 21:26:05 +0800

    creating admin context

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { assets } from "../assets/assets_admin/assets";
+
+const Navbar = () => {
+  return (
+    <div>
+      <img src={assets.admin_logo} alt="" srcset="" />
+    </div>
+  );
+};
+
+export default Navbar;
