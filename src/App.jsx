b23a6429c699d661a210d5a1c8006a93a5ee6ed1
mdarf <mdarfinji45@gmail.com>
2024-12-17 21:45:58 +0800

    basic layout for admin added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,12 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { AdminContext } from "./context/AdminContext";
 import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Admin/Dashboard";
+import AllAppointment from "./pages/Admin/AllAppointment";
+import AddDoctor from "./pages/Admin/AddDoctor";
+import DoctorList from "./pages/Admin/DoctorList";
 
 const App = () => {
   const { aToken } = useContext(AdminContext);
@@ -12,6 +18,19 @@ const App = () => {
     <div className="bg-[#F8F9FD]">
       <ToastContainer />
       <Navbar></Navbar>
+      <div className="flex items-start">
+        <Sidebar></Sidebar>
+        <Routes>
+          <Route path="/" element={<></>} />
+          <Route path="/admin-dashboard" element={<Dashboard></Dashboard>} />
+          <Route
+            path="/all-appointments"
+            element={<AllAppointment></AllAppointment>}
+          />
+          <Route path="/add-doctor" element={<AddDoctor></AddDoctor>} />
+          <Route path="/doctor-list" element={<DoctorList></DoctorList>} />
+        </Routes>
+      </div>
     </div>
   ) : (
     <div>
