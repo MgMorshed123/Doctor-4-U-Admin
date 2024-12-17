1eafa616a4a30a14e6578cab03d3e9f74d9d2221
mdarf <mdarfinji45@gmail.com>
2024-12-17 19:38:13 +0800

    Login Done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [state, setState] = useState("Admin");
+
+  return (
+    <form className="min-h-[80vh] flex items-center">
+      <div className="flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-[#5E5E5E] text-sm  shadow-lg ">
+        <p className="text-2xl font-semibold m-auto ">
+          <span className="text-primary">{state}</span> Login{" "}
+        </p>
+
+        <div className="w-full">
+          <p>Email</p>
+          <input
+            className="border border-[#DADADA] rounded w-full p-2 mt-1"
+            type="email"
+            required
+          />
+        </div>
+
+        <div className="w-full">
+          <p>Password</p>
+          <input
+            className="border border-[#DADADA] rounded w-full p-2 mt-1"
+            type="password"
+            required
+          />
+        </div>
+
+        <button className="bg-primary text-white w-full py-2 rounded-md text-base">
+          Login
+        </button>
+        {state === "Admin" ? (
+          <p className="cursor-pointer">
+            Doctor Login{" "}
+            <span className="text-blue-700" onClick={() => setState("Doctor")}>
+              Click Here{" "}
+            </span>
+          </p>
+        ) : (
+          <p className="cursor-pointer">
+            Admin Login{" "}
+            <span className="text-blue-700" onClick={() => setState("Admin")}>
+              Click Here{" "}
+            </span>
+          </p>
+        )}
+      </div>
+    </form>
+  );
 };
 
 export default Login;
