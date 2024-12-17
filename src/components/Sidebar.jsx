1a3d73f0f4cd3a0c0a15f89719abe195b921a454
mdarf <mdarfinji45@gmail.com>
2024-12-17 22:04:17 +0800

    creating sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { useContext } from "react";
+import { AdminContext } from "../context/AdminContext";
+import { NavLink } from "react-router-dom";
+import { assets } from "../assets/assets_admin/assets";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const { aToken } = useContext(AdminContext);
+
+  return (
+    <div>
+      {aToken && (
+        <ul>
+          <NavLink>
+            <img src={assets.home_icon} alt="" srcset="" />
+            <p>Dashboard</p>
+          </NavLink>
+
+          <NavLink>
+            <img src={assets.home_icon} alt="" srcset="" />
+            <p>Dashboard</p>
+          </NavLink>
+
+          <NavLink>
+            <img src={assets.home_icon} alt="" srcset="" />
+            <p>Dashboard</p>
+          </NavLink>
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default Sidebar;
