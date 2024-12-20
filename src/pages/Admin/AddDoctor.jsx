5a36a5c5bd1792384819aaf9cd5e69f76eff493a
mdarf <mdarfinji45@gmail.com>
2024-12-20 13:32:33 +0800

    creating add doctor ui

--- src/pages/Admin/AddDoctor.jsx
+++ src/pages/Admin/AddDoctor.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { assets } from "../../assets/assets_admin/assets";
 
 const AddDoctor = () => {
-  return <div>AddDoctor</div>;
+  return (
+    <form>
+      <p>Add Doctor</p>
+
+      <div>
+        <div>
+          <label htmlFor="doc-img">
+            <img src={assets.upload_area} alt="" srcset="" />
+          </label>
+
+          <input type="file" name="" id="" />
+          <p>
+            Upload Doctor <br /> Picture{" "}
+          </p>
+        </div>
+      </div>
+    </form>
+  );
 };
 
 export default AddDoctor;
